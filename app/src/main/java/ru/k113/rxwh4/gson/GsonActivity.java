ca75e9647c1b76648d078e904ad1171eb4f75c33
MakcPetrov <k-113@yandex.ru>
2019-06-04 15:24:13 +0300

    0

---
+++ app/src/main/java/ru/k113/rxwh4/gson/GsonActivity.java
@@ -0,0 +1,30 @@
+package ru.k113.rxwh4.gson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import ru.k113.rxwh4.R;
+
+public class GsonActivity extends AppCompatActivity {
+
+    private static final String TAG = "GsonActivity";
+    String json = "{\"time_of_year\": \"summer\", \"year\": 2019"+"}";
+
+    Gson gson = new GsonBuilder().create();
+
+    Season mSeason = gson.fromJson(json,Season.class);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gson);
+
+        Log.d(TAG,json);
+        Log.d(TAG, "onCreate: " + mSeason.time_of_year);
+        Log.d(TAG, "onCreate: " + mSeason.year);
+    }
+}
---
+++ app/src/main/java/ru/k113/rxwh4/gson/Season.java
@@ -0,0 +1,16 @@
+package ru.k113.rxwh4.gson;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class Season {
+
+    @Expose
+    @SerializedName("time_of_year")
+    public String time_of_year;
+
+    @Expose
+    @SerializedName("year")
+    public String year;
+}
